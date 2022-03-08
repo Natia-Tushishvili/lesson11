1f5f840e47f5edd4f989e87d3982f76f2901b140
Natia-Tushishvili <tushisvhilin03@gmail.com>
2022-03-08 13:51:23 -0800

    add script.js

---
+++ script.js
@@ -0,0 +1,82 @@
+
+// davaleba-1
+
+// let user={
+//     firstname:'giorgi',
+//     lastname:'smith',
+//     age:25,
+//     studentstatus: active
+// }
+// console.log(user.studentstatus);
+
+
+
+// davaleba-2
+
+// let numbers=[6, 14, 87, 54,9,26, 48];
+// for(let x=0; x < numbers.length;  x++){
+//    console.log(numbers[x]);
+// }
+
+
+
+// davaleba-3
+
+// let numbers=[ 5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];
+
+// for(let x of numbers){
+//     if(x>5)
+
+// console.log(x);
+// }
+
+
+// davaleba-4
+
+// let user = {
+// 	name: 'giorgi',
+// 	age:  20,
+// 	studentstatus:' active'
+// };
+
+// if(user.age>18 && user.studentstatus=='active'){
+//     console.log('hello');
+// }
+//  else if( user.name='giorgi'){
+//     console.log('hello giorgi');
+// }
+// else if(user.studentstatus==active || user.age<25){
+//     console.log('hello world');
+// }else {
+//     console.log('error');
+// }
+
+
+
+// davaleba-5  ver gavakete
+
+// let array = ['watermelon', 'pear', 10, 45, 50,'apple', 'ananas'] ;
+
+// for(let x of array ){
+//     if( typeof x==String){
+//         console.log(x);
+//     }
+// }
+
+// davaleba-6
+
+// let users=[
+//     {username:'test1', status:false},
+//     {username:'test2', status:false},
+//     {username:'test3', status:true}
+// ];
+
+// for(let username of users ){
+//     if(username.status==true){
+//         console.log(username);
+//     }
+// }
+
+
+
+
